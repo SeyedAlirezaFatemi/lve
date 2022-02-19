09e927abdf03e961b40bcdc2643895fd685340d1
Alireza Fatemi <alirezafa@rocketmail.com>
2022-02-19 13:14:03 +0200

    ✨ Create pipeline and device in App.

--- vulkan-engine/first_app.hpp
+++ vulkan-engine/first_app.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
+#include "lve_device.hpp"
+#include "lve_pipeline.hpp"
 #include "lve_window.hpp"
 
 namespace lve {
     class FirstApp {
-    public:
+       public:
         static constexpr int WIDTH = 1280;
         static constexpr int HEIGHT = 720;
 
         void run();
 
-    private:
+       private:
         LVEWindow lveWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
+        LVEDevice lveDevice{lveWindow};
+        LVEPipeline lvePipeline{lveDevice,
+                                "..\\..\\..\\..\\vulkan-engine\\shaders\\simple_shader.vert.spv",
+                                "..\\..\\..\\..\\vulkan-engine\\shaders\\simple_shader.frag.spv",
+                                LVEPipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
     };
-} // namespace lve
+}  // namespace lve
