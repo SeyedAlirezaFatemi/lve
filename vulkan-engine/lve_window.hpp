50c2f8025108cbd93b2c000cc6f01667ba604e2e
Alireza Fatemi <alirezafa@rocketmail.com>
2022-02-19 23:50:23 +0200

    ✨ Added getExtent method.

--- vulkan-engine/lve_window.hpp
+++ vulkan-engine/lve_window.hpp
@@ -18,6 +18,9 @@ namespace lve {
         LVEWindow &operator=(const LVEWindow &) = delete;
 
         bool shouldClose() { return glfwWindowShouldClose(window); };
+        VkExtent2D getExtent() {
+            return {static_cast<uint32_t>(width), static_cast<uint32_t>(height)};
+        }
 
         void createWindowSurface(VkInstance instance, VkSurfaceKHR *surface);
 
