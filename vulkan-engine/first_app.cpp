5b33d0c621a1c0ec402bab7f401962a308908664
Alireza Fatemi <alirezafa@rocketmail.com>
2022-02-20 20:44:52 +0200

    ✨ Added createCommandBuffers and drawFrame.

--- vulkan-engine/first_app.cpp
+++ vulkan-engine/first_app.cpp
@@ -1,5 +1,6 @@
 #include "first_app.hpp"
 
+#include <array>
 #include <stdexcept>
 
 namespace lve {
@@ -13,7 +14,10 @@ namespace lve {
     void FirstApp::run() {
         while (!lveWindow.shouldClose()) {
             glfwPollEvents();  // Poll window events
+            drawFrame();
         }
+        // CPU will block until all GPU operations have completed
+        vkDeviceWaitIdle(lveDevice.device());
     }
 
     void FirstApp::createPipelineLayout() {
@@ -48,6 +52,80 @@ namespace lve {
             pipelineConfig);
     }
 
-    void FirstApp::createCommandBuffers() {}
-    void FirstApp::drawFrame() {}
+    void FirstApp::createCommandBuffers() {
+        // lveSwapChain.imageCount() will likely be either 2 or 3 depending on if the device
+        // supports double or triple buffering. Each command buffer is going to draw to a different
+        // frame buffer.
+        commandBuffers.resize(lveSwapChain.imageCount());
+
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        // Command pools are opaque objects that command buffer memory is allocated from.
+        allocInfo.commandPool = lveDevice.getCommandPool();
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+        if (vkAllocateCommandBuffers(lveDevice.device(), &allocInfo, commandBuffers.data()) !=
+            VK_SUCCESS) {
+            throw std::runtime_error("Failed to allocate command buffers");
+        }
+
+        // We need to record our draw commands to each buffer
+        for (size_t i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to begin recording command buffer");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = lveSwapChain.getRenderPass();
+            // Which frame buffer this render pass writes in
+            renderPassInfo.framebuffer = lveSwapChain.getFrameBuffer(i);
+
+            // Setup render area
+            // The area where the shader loads and stores will take place.
+            renderPassInfo.renderArea.offset = {0, 0};
+            renderPassInfo.renderArea.extent = lveSwapChain.getSwapChainExtent();
+
+            // Set the clear values
+            // This corresponds to what we want the initial values of the frame buffer attachments
+            // cleared to.
+            // Index 0 is the color attachment and index 1 is the depth attachment.
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = {0.1f, 0.1f, 0.1f, 0.1f};
+            clearValues[1].depthStencil = {1.0f, 0};
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            // VK_SUBPASS_CONTENTS_INLINE signals that the subsequent render pass commands will be
+            // directly embedded in the primary command buffer itself and no secondary commands will
+            // be used.
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+            lvePipeline->bind(commandBuffers[i]);
+            // Draw 3 vertices and only one instance.
+            // Instances can be used when you want to draw multiple copies of the same vertex data.
+            // Can be used in rendering particle systems.
+            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+            vkCmdEndRenderPass(commandBuffers[i]);
+
+            if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to record command buffer");
+            }
+        }
+    }
+    void FirstApp::drawFrame() {
+        uint32_t imageIndex;
+        auto result = lveSwapChain.acquireNextImage(&imageIndex);
+        if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("Failed to acquire swap chain image");
+        }
+        result = lveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("Failed to present swap chain image");
+        }
+    }
 }  // namespace lve
